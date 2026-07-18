3ed20cac87edf3f248c54a7eb1a1416bbb0e7436
rishikesh reddy <sairishikeshreddyvemireddy@gmail.com>
2026-07-18 11:57:17 +0530

    commit changes in this

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,29 @@
-// alert("Hello from JS");
-console.log("Hello from JS");
+var users = [
+    {
+        "name": "mahesh",
+        "gender": "male",
+        "image": "mahesh.jpg",
+        "email": "maheshbabu@vnrvjiet.in"
+    },
+       { "name": "jhon",
+        "gender": "female",
+        "image": "john.png",
+        "email": "jhon@example.com"
+}
+]
+
+var id = 0;
+
+function toggleUser() {
+    id = (id+1)%2;
+
+    var userImage = document.getElementById("userImage");
+    userImage.src = users[id].image;
+    var name = document.getElementById("name");
+    name.innerHTML = users[id].name;
+    var email = document.getElementById("email");
+    email.innerHTML = users[id].email;
+    var gender = document.getElementById("gender");
+    gender.innerHTML = users[id].gender;
+}
+    
